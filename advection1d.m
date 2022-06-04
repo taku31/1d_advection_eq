d68540239dfd2f01dc364f58086feff534e4ed4e
naka <37662336+taku31@users.noreply.github.com>
2022-06-04 19:39:19 +0900

    Add files via upload

---
+++ advection1d.m
@@ -0,0 +1,112 @@
+% 初期化
+clearvars;
+
+% パラメーター
+Lx = 4;% 計算領域
+dLx = 0.1;% 要素サイズ
+dt = 0.05;
+nx = Lx / dLx + 1;% 要素数
+c = 1;% 移流速度
+t_max = 3;
+iter_max = t_max / dt;
+dt_pos = 0.2;% 画像出力間隔
+
+% クーラン数
+nu = c * dt / dLx;
+
+% 座標の生成
+x = 0 : dLx : Lx;
+
+% 配列確保
+q_exact = zeros(nx, 1);
+q = zeros(nx, 1);
+
+% 初期条件の設定
+for i =  1 : nx
+
+    if abs(x(i) - 1) <= 10^-6
+        q_exact(i) = 1;
+        q(i) = 1;
+    elseif x(i)  < 1
+        q_exact(i) = 1;
+        q(i) = 1;
+    else
+        q_exact(i) = 0;
+        q(i) = 0;
+    end
+
+end
+
+% 移流スキームの選択
+method = '1st order UDS';% 1次精度風上差分法
+%method = '2nd order CDS';% 2次精度中心差分法
+
+% 時間発展
+for iter = 1 : iter_max
+
+    time = iter * dt;
+    q_old = q;
+
+    % 解析解
+    for i = 1 : nx
+        if abs(x(i) - (1 + c * time)) < 10^-6
+            q_exact(i) = 1;
+        elseif x(i) - (1 + c * time) < 0
+            q_exact(i) = 1;
+        else
+            q_exact(i) = 0;
+        end
+    end
+
+    % 数値解
+    for i = 2 : nx - 1 % 境界要素は固定として計算は飛ばす。
+        switch method
+
+            case '1st order UDS'
+
+                q(i) = q_old(i) - nu * (q_old(i) - q_old(i - 1));% 1次精度風上差分
+
+
+            case '2nd order CDS'
+
+                q(i) = q_old(i) - 0.5 * nu * (q_old(i + 1) - q_old(i - 1));% 2次精度中心差分
+
+            otherwise
+
+                error("選択された手法：" + method + "はサポートしていないです。" + ...
+                    "1st order UDS, 2nd order CDSから選択してください。");
+        end
+    end
+
+    % コマンドウィンドウへの出力
+    txt = ['iter = ', num2str(iter), ' / ', num2str(iter_max)];
+    disp(txt);
+
+    % リアルタイム可視化
+    filename = [method, ', dt = ', num2str(dt), ', dx = ', num2str(dLx),', c = ', num2str(c),', nu = ', num2str(nu),'.gif'];
+    fignum = 1;
+    plot(x, q_exact, x, q)
+    title(['time = ', num2str(time, '%.3f')]);
+    set(gca, 'FontName', 'Times New Roman', 'FontSize', 12);
+    axis equal; axis tight; axis on;
+    fig=gcf;
+    fig.Color='white';
+    xlim([0 4]);
+    ylim([0 1.5]);
+    xlabel('x')
+    ylabel('q')
+
+    legtxt = [method, ', dt = ', num2str(dt), ', dx = ', num2str(dLx),', c = ', num2str(c),', nu = ', num2str(nu)];
+    legend('exact', legtxt,'Location','southwest')
+    legend('boxoff')
+
+    frame = getframe(fignum);
+    im = frame2im(frame);
+    [imind, cm] = rgb2ind(im, 256);
+    if time == dt_pos
+        imwrite(imind, cm, filename, 'gif', 'DelayTime', 0.2, 'Loopcount', inf);
+    elseif rem(time, dt_pos) == 0
+        imwrite(imind, cm, filename, 'gif', 'DelayTime', 0.2, 'WriteMode', 'append');
+    end
+
+end
